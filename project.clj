d4bcca47d33ab7e390364ed0e39584be21a489cf
Thomas Meier <thomas.meier@citrine.it>
2014-06-04 17:17:14 -0500

    updated description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject haversine "0.1.0"
   :description "Find the great circle distance between two points of lat/log on a sphere (approximately Earth)"
-  :url "http://citrine.it/"
+  :url "https://github.com/ThomasMeier/haversine"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
